e0ba1d10fe3a5ff3d5ee7e76ba7e2d9a1cb5a2be
Vishal180309 <2305112110077@paruluniversity.ac.in>
2025-02-10 10:45:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,56 @@
 //complete this code
-class Person {}
+class Person {
+    constructor(name, age) {
+        this._name = name;
+        this._age = age;
+    }
 
-class Student extends Person {}
+    get name() {
+        return this._name;
+    }
 
-class Teacher extends Person {}
+    set age(age) {
+        this._age = age;
+    }
 
-// Do not change the code below this line
-window.Person = Person;
-window.Student = Student;
-window.Teacher = Teacher;
+    get age() {
+        return this._age;
+    }
+}
+
+class Student extends Person {
+    constructor(name, age) {
+        super(name, age);
+    }
+
+    study() {
+        console.log(`${this.name} is studying`);
+    }
+}
+
+class Teacher extends Person {
+    constructor(name, age) {
+        super(name, age);
+    }
+
+    teach() {
+        console.log(`${this.name} is teaching`);
+    }
+}
+
+document.getElementById('create-person-button').addEventListener('click', function() {
+    const person = new Person('John Doe', 30);
+    console.log(person.name);
+    person.age = 31;
+    console.log(person.age);
+});
+
+document.getElementById('create-student-button').addEventListener('click', function() {
+    const student = new Student('Jane Doe', 20);
+    student.study();
+});
+
+document.getElementById('create-teacher-button').addEventListener('click', function() {
+    const teacher = new Teacher('Bob Smith', 40);
+    teacher.teach();
+});
